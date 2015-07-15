c7f8bf81e0d3ec80f01ecfb7a728a08efb6c6010
chma <christophe.maillard@rocketmail.com>
2015-07-16 00:02:07 +0200

    [1.0] Sequence repository improved

--- mongo/src/main/java/org/jrestful/data/repositories/support/sequence/SequenceRepository.java
+++ mongo/src/main/java/org/jrestful/data/repositories/support/sequence/SequenceRepository.java
@@ -3,10 +3,12 @@ package org.jrestful.data.repositories.support.sequence;
 import org.jrestful.data.documents.support.sequence.GenericSequencedDocument;
 import org.jrestful.data.documents.support.sequence.Sequence;
 import org.jrestful.data.repositories.support.GenericDocumentRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 /**
  * Manages auto-incremented sequenced.
  */
+@NoRepositoryBean
 public interface SequenceRepository extends GenericDocumentRepository<Sequence> {
 
   Sequence findNext(GenericSequencedDocument document);
