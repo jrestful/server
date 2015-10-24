fc5a12292c11e35984cb0a5b41e25322a757afea
chma <christophe.maillard@rocketmail.com>
2015-10-24 21:26:52 +0200

    [1.0.0] DateUtils improved

--- core/src/main/java/org/jrestful/util/DateUtils.java
+++ core/src/main/java/org/jrestful/util/DateUtils.java
@@ -8,14 +8,23 @@ import java.util.Date;
  */
 public final class DateUtils {
 
-  public static Date addToNow(int field, int amount) {
+  public static Date add(Date date, int field, int amount) {
     Calendar calendar = Calendar.getInstance();
+    calendar.setTime(date);
     calendar.add(field, amount);
     return calendar.getTime();
   }
 
+  public static Date addToNow(int field, int amount) {
+    return add(new Date(), field, amount);
+  }
+
+  public static Date remove(Date date, int field, int amount) {
+    return add(date, field, -amount);
+  }
+
   public static Date removeToNow(int field, int amount) {
-    return addToNow(field, -amount);
+    return remove(new Date(), field, amount);
   }
 
   private DateUtils() {
