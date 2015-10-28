2a322ec104c2bf781ed1798c6b8799b98dec4505
chma <christophe.maillard@rocketmail.com>
2015-10-28 18:02:01 +0100

    [1.0.0] DateUtils improved

--- core/src/main/java/org/jrestful/util/DateUtils.java
+++ core/src/main/java/org/jrestful/util/DateUtils.java
@@ -1,12 +1,46 @@
 package org.jrestful.util;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * Operations on dates.
  */
 public final class DateUtils {
+
+  private interface PredefinedDateFormat {
+
+    String format(Date date);
+
+    Date parse(String date) throws ParseException;
+
+  }
+
+  public static final PredefinedDateFormat RFC_1123 = new PredefinedDateFormat() {
+
+    @Override
+    public String format(Date date) {
+      return build().format(date);
+    }
+
+    @Override
+    public Date parse(String date) throws ParseException {
+      return build().parse(date);
+    }
+
+    private DateFormat build() {
+      SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+      dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+      dateFormat.setLenient(false);
+      return dateFormat;
+    }
+
+  };
 
   public static Date add(Date date, int field, int amount) {
     Calendar calendar = Calendar.getInstance();
