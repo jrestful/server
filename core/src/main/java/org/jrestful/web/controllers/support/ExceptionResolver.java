e798073c3906381f9068b5b112ee1c8b4d36a84c
chma <christophe.maillard@rocketmail.com>
2016-03-05 22:39:48 +0100

    [1.1.0] Adding X-API-Version header when NoHandlerFoundException

--- core/src/main/java/org/jrestful/web/controllers/support/ExceptionResolver.java
+++ core/src/main/java/org/jrestful/web/controllers/support/ExceptionResolver.java
@@ -1,10 +1,12 @@
 package org.jrestful.web.controllers.support;
 
-import static org.jrestful.web.beans.RestResponse.badRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.jrestful.business.exceptions.HttpStatusException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,19 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class ExceptionResolver {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionResolver.class);
+
+  private final String apiVersion;
+
+  @Autowired
+  public ExceptionResolver(@Value("#{appProps['app.apiVersion']}") String apiVersion) {
+    this.apiVersion = apiVersion;
+  }
 
   @ExceptionHandler(HttpStatusException.class)
   public ResponseEntity<?> resolveHttpStatusException(HttpStatusException e) {
@@ -25,16 +35,20 @@ public class ExceptionResolver {
   }
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<?> resolveException(Exception e) throws Exception {
+  public ResponseEntity<?> resolveException(Exception e, HttpServletResponse response) throws Exception {
     if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
       LOGGER.warn("@ResponseStatus annotated Exception catched, rethrowing", e);
       throw e;
     } else if (e instanceof AccessDeniedException) {
       LOGGER.warn("AccessDeniedException catched, rethrowing", e);
       throw e;
+    } else if (e instanceof NoHandlerFoundException) {
+      LOGGER.warn("NoHandlerFoundException catched, returning " + HttpStatus.NOT_FOUND, e);
+      response.setHeader("X-API-Version", apiVersion);
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     } else {
-      LOGGER.error("Exception catched, returning " + HttpStatus.BAD_REQUEST, e);
-      return badRequest();
+      LOGGER.error("Exception catched, returning " + HttpStatus.INTERNAL_SERVER_ERROR, e);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
